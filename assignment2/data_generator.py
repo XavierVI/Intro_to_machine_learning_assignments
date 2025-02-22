2a05507f6c1cff1b874c0116b7d7feb86faf473e
xavier <vigenesisvi@gmail.com>
2025-02-22 10:01:10 -0700

    fixed data_generator.py

--- assignment2/data_generator.py
+++ assignment2/data_generator.py
@@ -2,11 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def make_classification(d, n, u=5, random_state=1):
+def make_classification(d, n, u=5, threshold=0.01, random_state=1):
     """
     This function generates a set of points in a
     'd' dimensional real space. Each point will lie in
     the boundary [-u, u] in each direction of the space.
+
+    this functions returns the data points and labels in a single
+    2D array, and it returns the normal vector used to define the
+    hyperplane.
 
     d: the dimension of the space
     
@@ -15,6 +19,9 @@ def make_classification(d, n, u=5, random_state=1):
     u: the range in which the data points will be generated.
     Should be no greater than 5.
 
+    threshold: the maximum distance such that if |np.dot(a^T, x)| >= threshold,
+    the point is kept in the dataset. Otherwise it is removed.
+
     random_state: the seed for generating random points
     """
     # create a random number generator based on
@@ -22,7 +29,8 @@ def make_classification(d, n, u=5, random_state=1):
     rng = np.random.default_rng(random_state)
     # create a random normal vector to define a hyperplane
     normal_vector = rng.random(d)
-    
+    normal_vector /= np.linalg.norm(normal_vector)
+
     # uniform set of joint angles
     data_points = rng.uniform(
         low=-u,  # lower bound
@@ -33,36 +41,38 @@ def make_classification(d, n, u=5, random_state=1):
 
     # computing the inner products
     inner_products = np.dot(data_points, normal_vector)
-   
+
     # removing points on the line
-    points_near_line = np.abs(inner_products) >= 0.00001
+    points_near_line = np.abs(inner_products) >= threshold
     data_points = data_points[points_near_line]
     inner_products = inner_products[points_near_line]
     n = inner_products.shape[0]
-    
+
     # generating the labels for each point
     labels = np.where(inner_products < 0, -1, 1)
 
     # appending the labels to the data points
-    data_points = np.hstack((data_points, labels.reshape(n, 1)))
+    data = np.hstack((data_points, labels.reshape(n, 1)))
 
-    return (data_points, normal_vector)
+    return (data, normal_vector)
 
 
+d = 2
+u = 1
+treshold = 0.05
+n = 1000
 
-data_points, normal_vector = make_classification(2, 100)
-print(data_points)
-print(data_points.shape[0])
+data_points, normal_vector = make_classification(d=d, n=n, u=u, threshold=0.15)
+print(data_points[:5])
+print(data_points.shape)
 
-x1_coords = np.linspace(-5, 5, 100)
-x2_coodrs = np.linspace(-5, 5, 100)
-x_coords = np.column_stack((x1_coords, x2_coodrs))
-line = np.dot(x_coords, normal_vector)
+# plotting line and data points
+x1_vals = np.linspace(-u, u, 100)
+# solving for x2 values x2 = (-a1 / a2) * x1
+x2_vals = - (normal_vector[0] / normal_vector[1]) * x1_vals
 
-plt.plot(data_points[:, 0], data_points[:, 1], 'xr')
-plt.plot(x_coords, line, '-b')
+plt.plot(data_points[:, 0], data_points[:, 1], 'xg')
+plt.plot(x1_vals, x2_vals, '-b')
 plt.xlabel('x1')
 plt.ylabel('x2')
-plt.xlim([-5.5, 5.5])
-plt.ylim([-5.5, 5.5])
 plt.show()

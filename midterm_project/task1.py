3d845254c076c609aff81479322dcc0150f54a5b
xavier <vigenesisvi@gmail.com>
2025-03-14 09:37:43 -0600

    updated code for task 1

--- midterm_project/task1.py
+++ midterm_project/task1.py
@@ -1,35 +1,31 @@
 import numpy as np
 
 
-class Node:
-    def __init__(self, condition):
-        """
-        @args:
-        - condition: a boolean expression associated with the node
-        """
-        self.left_child = None
-        self.right_child = None
-        self.condition = condition
-        pass
     
-    def add_node(self, node: Node, left: bool):
-
-        if left:
-            self.left_child = node
-        else:
-            self.right_child = node
-    
-
 class BST:
     """
     This class is a binary search tree.
+
+    @args:
+    - condition: a boolean expression associated with the node
     """
-    def __init__(self, condition):
+    def __init__(self):
         """
         @args:
         - condition: a boolean expression associated with the node
         """
-        self.root = Node(condition)
+        self.condition = None
+        self.left_child = None
+        self.right_child = None
+
+    def set_condition(self, exp: lambda: bool):
+        self.condition = exp
+
+    def set_left_child(self, node):
+        self.left_child = node
+
+    def set_right_child(self, node):
+        self.right_child = node
     
 
 
@@ -37,25 +33,33 @@ class RegressionTree:
     def __init__(self, max_height: int, leaf_size: int):
         pass
 
-    
+
     def decision_path(X):
         pass
 
     def fit(self, X, y):
+        # index of the feature
+        feature_idx = 0
         # used to track which node to visit next
-        stack = []
+        node_queue = []
+        # initialize the root node
+        node = BST()
 
-        while not self.__stopping_criteria():
+        while not self.stopping_criteria():
             # compute the index which has the lowest
             # sum of squares value (excluding end points)
-            np.argmin(self.__sum_of_squares(y[1:-1]))
+            sample_idx = np.argmin(self.sum_of_squares(y[1:-1]))
+            
+            # use the index to split the data at X[idx]
+            # this is also where we set the condition of the node
+            condition = lambda x: x <= X[sample_idx, feature_idx]
 
-        pass
 
-    def __sum_of_squares(self, y):
+
+    def sum_of_squares(self, y):
         return y.size[0] *  np.std(y)
 
-    def __stopping_criteria(self):
+    def stopping_criteria(self):
         """
         This function will calculate if we should stop building
         the tree based on the tree's current structure and the given
@@ -64,5 +68,6 @@ class RegressionTree:
         return False
 
 
-model = RegressionTree(0)
-model.__sum_of_squares([0])
+model = Node(lambda x: x <= 3)
+
+

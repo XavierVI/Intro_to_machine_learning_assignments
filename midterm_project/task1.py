2edd1dcef6a3fddfeef923aa3724a7cc9faa96bc
xavier <vigenesisvi@gmail.com>
2025-03-14 11:24:45 -0600

    modified fit() to sort the data

--- midterm_project/task1.py
+++ midterm_project/task1.py
@@ -9,10 +9,11 @@ class BST:
     @args:
     - condition: a boolean expression associated with the node
     """
-    def __init__(self):
+    def __init__(self, min_idx, max_idx, feature):
         """
         @args:
-        - condition: a boolean expression associated with the node
+        - condition: a boolean expression associated with the node.
+        This condition should be used to determine how the data is split.
         """
         self.condition = None
         self.left_child = None
@@ -38,21 +39,62 @@ class RegressionTree:
         pass
 
     def fit(self, X, y):
+        data = np.column_stack((X, y))
+        np.sort(data)
+        X = data[:, 0:-1]
+        y = data[:, -1]
+        num_of_features = X.size[1]
+
         # index of the feature
         feature_idx = 0
-        # used to track which node to visit next
-        node_queue = []
+        
         # initialize the root node
         node = BST()
 
-        while not self.stopping_criteria():
+        # used to track which node to visit next
+        node_queue = []
+        # add root node to the queue
+        node_queue.append({
+            'node': node,
+            'min_idx': 1, # ignore end point
+            'max_idx': -1, # -1 -> last index of X
+            'feature': 0
+        })
+
+
+        while len(node_queue) > 0:
+            # get a node from the queue
+            node_dict = node_queue.pop(0)
             # compute the index which has the lowest
             # sum of squares value (excluding end points)
-            sample_idx = np.argmin(self.sum_of_squares(y[1:-1]))
+            sample_idx = np.argmin(
+                self.sum_of_squares(
+                    y[node_dict['min_idx'] : node_dict['max_idx']]
+                )
+            )
             
             # use the index to split the data at X[idx]
             # this is also where we set the condition of the node
-            condition = lambda x: x <= X[sample_idx, feature_idx]
+            node.condition = lambda x: x <= X[sample_idx, feature_idx]
+            
+            # create the children for the node
+            node.left_child = BST()
+            node.right_child = BST()
+            feature = 0 if feature_idx + 1 > num_of_features else feature_idx + 1
+            # add them to the queue
+            node_queue.append({
+                'node': node.left_child,
+                'min_idx': node_dict['min_idx'],
+                'max_idx': sample_idx,
+                'feature': feature
+            })
+            node_queue.append({
+                'node': node.right_child,
+                'min_idx': sample_idx,
+                'max_idx': node_dict['max_idx'],
+                'feature': feature
+            })
+
 
 
 
@@ -67,7 +109,5 @@ class RegressionTree:
         """
         return False
 
-
-model = Node(lambda x: x <= 3)
 
 

6ea49f9658226148b72e5b2dc8324f0475b2f400
xavier <vigenesisvi@gmail.com>
2025-03-16 19:48:10 -0600

    finished code for building a regression tree

--- midterm_project/task1.py
+++ midterm_project/task1.py
@@ -1,24 +1,21 @@
 import numpy as np
 
-
-    
-class BST:
-    """
-    This class is a binary search tree.
-
-    @args:
-    - condition: a boolean expression associated with the node
-    """
-    def __init__(self, min_idx, max_idx, feature):
+class Node:
+    def __init__(
+        self,
+        left_child=None, right_child=None,
+        condition: lambda: bool=None, value: float=None):
         """
         @args:
         - condition: a boolean expression associated with the node.
         This condition should be used to determine how the data is split.
         """
         self.condition = None
-        self.left_child = None
-        self.right_child = None
-
+        self.left_child = left_child
+        self.right_child = right_child
+        self.condition = condition
+        self.value = value
+    
     def set_condition(self, exp: lambda: bool):
         self.condition = exp
 
@@ -29,11 +26,52 @@ class BST:
         self.right_child = node
     
 
+class BST:
+    """
+    This class is a binary search tree.
+
+    @args:
+    - condition: a boolean expression associated with the node
+    """
+    def __init__(self):
+        self.root_node = Node()
+
+    def get_height(self, node):
+        if node == None:
+            return 0
+
+        return 1 + max(
+            self.get_height(node.left_child),
+            self.get_height(node.right_child)
+        )
+
+    def get_num_of_leaves(self):
+        num_of_leaves = 0
+        node_stack = []
+        node_stack.push(self.root_node)
+
+        while len(node_stack) > 0:
+            node = node_stack.pop()
+
+            if node.left_child != None:
+                node_stack.push(node.left_child)
+
+            if node.right_child != None:
+                node_stack.push(node.right_child)
+
+            if node.left_child == None and node.right_child == None:
+                num_of_leaves += 1
+        
+        return num_of_leaves
+
+
 
 class RegressionTree:
     def __init__(self, max_height: int=None, leaf_size: int=None):
         self.max_height = max_height
         self.leaf_size = leaf_size
+        self.height = 0
+        self.bst = BST()
 
 
     def decision_path(X):
@@ -42,15 +80,20 @@ class RegressionTree:
     def fit(self, X, y):
         num_of_features = X.size[1]
         
-        # initialize the root node
-        node = BST()
+        # start at the root node
+        node = self.bst.root_node
+
+        # calculate the impurity of the whole dataset
+        sse = self.sum_of_squares(y)
 
         # used to track which node to visit next
         node_queue = []
         # add root node to the queue
         node_queue.append({
             'node': node,
+            'depth': 0,
             'mask': np.array([True] * X.size[0]),
+            'impurity': sse,
             'feature': 0
         })
 
@@ -58,35 +101,58 @@ class RegressionTree:
         while len(node_queue) > 0:
             # get a node from the queue
             node_dict = node_queue.pop(0)
+            curr_node = node_dict['node']
+            depth = node_dict['depth']
+            num_of_leaves = self.bst.get_num_of_leaves()
+            impurity = node_dict['impurity']
             feature_idx = node_dict['feature']
-            X_local = X['mask']
-            y_local = y['mask']
+            X_local = X[node_dict['mask']]
+            y_local = y[node_dict['mask']]
+
+            # determine if this node should be a leaf
+            if X_local.shape[0] < 3 or impurity == 0 or \
+                self.can_add_node(num_of_leaves, depth):
+                # set the value property
+                curr_node.value = np.mean(y_local)
+                # continue to the next iteration
+                continue
+
             # compute the index which has the lowest
             # sum of squares value
-            sample_idx = self.get_best_split(
-                X_local, y_local, feature_idx
-            )
+            sample_idx, left_sse, right_sse = \
+                self.get_best_split(X_local, y_local, feature_idx)
             
-            # use the index to split the data at X[idx]
-            # this is also where we set the condition of the node
-            node.condition = lambda x: x <= X[sample_idx, feature_idx]
-            
-            # create the children for the node
-            node.left_child = BST()
-            node.right_child = BST()
+            # use the index which splits the data at X[sample_idx, feature_idx]
+            # to set the condition property of the node
+            curr_node.condition = lambda x: x <= X[sample_idx, feature_idx]
+
             next_feature = 0 if feature_idx + 1 > num_of_features else feature_idx + 1
-            
-            # add them to the queue
-            node_queue.append({
-                'node': node.left_child,
-                'mask': X[:, feature_idx] < X[sample_idx, feature_idx],
-                'feature': next_feature
-            })
-            node_queue.append({
-                'node': node.right_child,
-                'mask': X[:, feature_idx] >= X[sample_idx, feature_idx],
-                'feature': next_feature
-            })
+
+            if num_of_leaves + 1 <= self.leaf_size:
+                # create a node and set it as the left child of curr_node
+                curr_node.left_child = Node()
+                # add left_child to the queue
+                node_queue.append({
+                    'node': curr_node.left_child,
+                    'depth': depth + 1,
+                    'mask': (X[:, feature_idx] < X_local[sample_idx, feature_idx])
+                            & node_dict['mask'],
+                    'impurity': left_sse,
+                    'feature': next_feature
+                })
+
+            if num_of_leaves + 2 <= self.leaf_size:
+                # create a node and set it as the right child of curr_node
+                curr_node.right_child = Node()
+                # add right_child to the queue
+                node_queue.append({
+                    'node': curr_node.right_child,
+                    'depth': depth + 1,
+                    'mask': (X[:, feature_idx] > X_local[sample_idx, feature_idx]) 
+                            & node_dict['mask'],
+                    'impurity': right_sse,
+                    'feature': next_feature
+                })
 
 
     def get_boolean_masks(self, X, sample_idx, feature_idx):
@@ -107,21 +173,40 @@ class RegressionTree:
         left_bool_mask, right_bool_mask = \
             self.get_boolean_masks(X, 1, feature_idx)
         best_sample_idx = 1
+
         # best sum of squares error
-        best_sse = self.sum_of_squares(y[left_bool_mask]) + \
-                   self.sum_of_squares(y[right_bool_mask])
-        for sample_idx in range(0, X.shape[0] - 1):
+        best_left_sse = self.sum_of_squares(y[left_bool_mask])
+        best_right_sse = self.sum_of_squares(y[right_bool_mask])
+
+        for sample_idx in range(2, X.shape[0] - 1):
             left_bool_mask, right_bool_mask = self.get_boolean_masks(
                 X, sample_idx, feature_idx
             )
-            sse = self.sum_of_squares(y[left_bool_mask]) + \
-                  self.sum_of_squares(y[right_bool_mask])
+            left_sse = self.sum_of_squares(y[left_bool_mask])
+            right_sse = self.sum_of_squares(y[right_bool_mask])
 
-            if sse < best_sse:
+            if left_sse + right_sse < best_left_sse + best_right_sse:
                 best_sample_idx = sample_idx
-                best_sse = sse
+                best_left_sse = left_sse
+                best_right_sse = right_sse
 
-        return best_sample_idx
+        return (best_sample_idx, best_left_sse, best_right_sse)
+
+    def can_add_node(self, num_of_leaves, node_depth):
+        """
+        This function will be used to help determine if a node should
+        be a leaf node. It specifically determines if the BST will not
+        exceed the hyperparameters of the BST, if any were given.
+
+        @args:
+        - num_of_leaves: the number of leaves in the tree.
+        - node_depth: the depth of a node in the BST.
+        """
+        return (
+            (self.leaf_size != None and num_of_leaves >= self.leaf_size)
+            or 
+            (self.max_height != None and self.max_height == node_depth)
+        )
 
 
     def sum_of_squares(self, y):
@@ -131,7 +216,7 @@ class RegressionTree:
         @args:
         - y: an array of labels
         """
-        return y.size[0] *  np.std(y)
+        return y.size[0] *  np.var(y)
 
     def stopping_criteria(self):
         """

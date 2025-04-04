d56216f3f28df98f84c431e916ad9f9484ece528
xavier <vigenesisvi@gmail.com>
2025-04-03 23:26:31 -0600

    added grid search

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -1,7 +1,9 @@
 import numpy as np
 import idx2numpy
 
-from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
@@ -41,6 +43,27 @@ def load_data(dir_path: str):
     return training_images, training_labels, testing_images, testing_labels
 
 
+def grid_search(params, n_components, X_train, y_train):
+    # sklearn pipeline (fashion)
+    pipeline = make_pipeline(
+        StandardScaler(),
+        PCA(n_components=n_components),
+        SVC(random_state=random_state),
+        verbose=False
+    )
+
+    gs = GridSearchCV(
+        estimator=pipeline,
+        param_grid=params,
+        scoring='accuracy',
+        cv=2,
+        n_jobs=2
+    )
+
+    gs.fit(X_train, y_train)
+
+    return gs.best_params_, gs.best_score_
+
 #### Loading the data into numpy arrays
 # TODO: turn the directory paths into command line arguments?
 mnist_data_dir_path = './mnist-data'
@@ -48,14 +71,28 @@ fashion_data_dir_path = './fashion-data'
 
 X_train, y_train, X_test, y_test = load_data(fashion_data_dir_path)
 
-#### sklearn pipeline (fashion)
+#### dictionary for grid search
+grid_search_params_linear = {
+    'svc__kernel': ['linear'],
+    'svc__C': [1, 10]
+}
 
-fashion_pipeline = make_pipeline(
-    StandardScaler(),
-    PCA(n_components=50),
-    SVC(kernel='poly', random_state=random_state),
-    verbose=True
-)
-fashion_pipeline.fit(X_train, y_train)
-print(f'Percentage of correctly identified samples: {fashion_pipeline.score(X_test, y_test)*100:.2f}')
+grid_search_params_rbf = {
+        'svc__kernel': ['rbf'],
+        'svc__C': [1, 10],
+        'svc__gamma': [1]
+}
 
+grid_search_params_poly = {
+    'svc__kernel': ['poly'],
+    'svc__C': [1, 10],
+    'svc__gamma': [1],
+    'svc__degree': [2]
+}
+
+PCA_values = [50, 100, 200]
+
+for pca_val in PCA_values:
+    print(grid_search(grid_search_params_linear, pca_val, X_train[:1000], y_train[:1000]))
+    print(grid_search(grid_search_params_rbf, pca_val, X_train[:1000], y_train[:1000]))
+    print(grid_search(grid_search_params_poly, pca_val, X_train[:1000], y_train[:1000]))

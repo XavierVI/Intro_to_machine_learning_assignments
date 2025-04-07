a3fab910a82f146713a0a80dcbfedc54c7af1546
xavier <vigenesisvi@gmail.com>
2025-04-07 13:56:36 -0600

    updated assignment 3

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -11,7 +11,10 @@ from sklearn.svm import SVC
 
 import os
 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+import sys
 
 def load_data(dir_path: str):
     """
@@ -70,32 +73,45 @@ def main():
     random_state = 1
    
     #### Dataset directory paths
-    mnist_data_dir_path = './mnist-data'
-    fashion_data_dir_path = './fashion-data'
+    if len(sys.argv) >= 2:
+        data_dir_path = sys.argv[1]
+        
+    else:
+        print("Please specify a path to the data using 'python assignment3.py /path/to/data")
+        print("or use ./path/to/data")
+        return 1
+
+    #### Dictionary for storing table data
+    table = {
+        'PCA Comps': [],
+        'Kernel': [],
+        'Accuracy': []
+    }
    
     #### Grid search parameters and PCA components
-    params_linear = {
-        'svc__kernel': ['linear'],
-        'svc__C': [1, 2, 4, 8, 12, 16, 32, 64]
-    }
-
-    params_rbf = {
-        'svc__kernel': ['rbf'],
-        'svc__C': [1, 2, 4, 8, 12, 16, 32, 64],
-        'svc__gamma': [1, 2, 4, 8, 16, 32, 64],
-    }
-
-    params_poly = {
-        'svc__kernel': ['poly'],
-        'svc__C': [1, 2, 4, 8, 12, 16, 32, 64],
-        'svc__gamma': [1, 2, 4, 8, 16, 32, 64],
-        'svc__degree': [2, 3, 4]
-    }
+    params = [
+        {
+            'svc__kernel': ['linear'],
+            'svc__C': [1, 2, 4, 8, 12, 16, 32, 64]
+        },
+        {
+            'svc__kernel': ['rbf'],
+            'svc__C': [1, 2, 4, 8, 12, 16, 32, 64],
+            'svc__gamma': [1, 2, 4, 8, 16, 32, 64],
+        },
+        {
+            'svc__kernel': ['poly'],
+            'svc__C': [1, 2, 4, 8, 12, 16, 32, 64],
+            'svc__gamma': [1, 2, 4, 8, 16, 32, 64],
+            'svc__degree': [2, 3, 4]
+        }
+    ]
    
     PCA_comps = [50, 100, 200]
 
     #### Loading data
-    X_train, y_train, X_test, y_test = load_data(mnist_data_dir_path)
+    X_train, y_train, X_test, y_test = load_data(data_dir_path)
+    class_names = np.unique(y_test)
 
 
     for n_comps in PCA_comps:
@@ -107,17 +123,40 @@ def main():
             verbose=False
         )
         print(f'Finding best hyperparameters for MNIST, n_comps = {n_comps}')
-        
-        #### Get the best model with a linear kernel
-        linear_model = grid_search(
-            pipeline, params_linear, X_train[:1000], y_train[:1000])
-        linear_accuracy = linear_model.score(X_test, y_test)
-        linear_cm = confusion_matrix(y_test, linear_model.predict(X_test))
-        print(linear_cm)
 
+        # 1 row, 3 columns of subplots
+        fig, axes = plt.subplots(1, 3, figsize=(18, 12))
 
-        
+        for i, param_list in enumerate(params):
+            #### Get the best model with parameters in param_list
+            model = grid_search(
+                pipeline, param_list, X_train[:1000], y_train[:1000])
+            accuracy = model.score(X_test, y_test)
+            cm = confusion_matrix(y_test, model.predict(X_test))
 
+            #### Adding values to the table
+            table['PCA Comps'].append(n_comps)
+            table['Kernel'].append(param_list['svc__kernel'][0])
+            table['Accuracy'].append(accuracy)
+            
+            
+            #### creating an image of a confusion matrix
+            sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
+                        xticklabels=class_names,
+                        yticklabels=class_names,
+                        ax=axes[i])
+            
+            axes[i].set_title(f'{param_list["svc__kernel"][0]} Kernel')
+            axes[i].set_ylabel('True label')
+            axes[i].set_xlabel('Predicted label')
+            
+        fig.suptitle(f'MNIST, PCA={n_comps}')
+        # plt.show()
+        plt.savefig(fname=f'./images/cm_pca_{n_comps}_{param_list['svc__kernel'][0]}_kernel')
+
+    #### Plotting the table
+    for n_comps, kernel, acc in zip(table['PCA Comps'], table['Kernel'], table['Accuracy']):
+        print(f"{n_comps} & {kernel} & {acc:.2f}  \\\\")
 
 
 if __name__ == '__main__':

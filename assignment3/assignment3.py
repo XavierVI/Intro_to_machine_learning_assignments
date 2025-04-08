7c895b1f9815c2afe81f2d96b6383cb810089efd
xavier <vigenesisvi@gmail.com>
2025-04-07 19:14:07 -0600

    updated assignment 3

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -10,6 +10,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.svm import SVC
 
 import os
+import time
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -21,6 +22,7 @@ def load_data(dir_path: str):
     
     dir_path: the path to the directory with the data
     """
+    print(f'Loading data stored in {dir_path }')
     num_of_training_samples = 60_000
     num_of_testing_samples = 10_000
 
@@ -51,7 +53,7 @@ def grid_search(pipeline, params, X_train, y_train):
         param_grid=params,
         scoring='accuracy',
         cv=2,
-        n_jobs=2,
+        n_jobs=10,
         # the model will return with the best parameters
         # if this value is True
         refit=True
@@ -60,59 +62,47 @@ def grid_search(pipeline, params, X_train, y_train):
     gs.fit(X_train, y_train)
 
     # return the pipeline with the best parameters
-    return gs.best_estimator_
-
-def find_best_hyper_params(pipeline, X_train, y_train, params):
-    # dictionaries for grid search
-    # TODO: add values for each parameter (at least 8 for each)
-
-    pass
+    return (gs.best_estimator_, gs.refit_time_)
 
 
-def main():
+def run_task(data_dir_path, dataset_name):
+    """Completes task 4 using mnist dataset"""
     random_state = 1
-   
-    #### Dataset directory paths
-    if len(sys.argv) >= 2:
-        data_dir_path = sys.argv[1]
-        
-    else:
-        print("Please specify a path to the data using 'python assignment3.py /path/to/data")
-        print("or use ./path/to/data")
-        return 1
+    subset = 10_000
 
-    #### Dictionary for storing table data
+    # Dictionary for storing table data
     table = {
         'PCA Comps': [],
         'Kernel': [],
-        'Accuracy': []
+        'Accuracy': [],
+        'Time Cost': []
     }
-   
-    #### Grid search parameters and PCA components
+
+    # Grid search parameters and PCA components
+    param_range = [1e-4, 0.1, 1, 100]
     params = [
         {
             'svc__kernel': ['linear'],
-            'svc__C': [1, 2, 4, 8, 12, 16, 32, 64]
+            'svc__C': param_range
         },
         {
             'svc__kernel': ['rbf'],
-            'svc__C': [1, 2, 4, 8, 12, 16, 32, 64],
-            'svc__gamma': [1, 2, 4, 8, 16, 32, 64],
+            'svc__C': param_range,
+            'svc__gamma': param_range,
         },
         {
             'svc__kernel': ['poly'],
-            'svc__C': [1, 2, 4, 8, 12, 16, 32, 64],
-            'svc__gamma': [1, 2, 4, 8, 16, 32, 64],
-            'svc__degree': [2, 3, 4]
+            'svc__C': param_range,
+            'svc__gamma': param_range,
+            'svc__degree': [2, 3, 4, 5]
         }
     ]
-   
+
     PCA_comps = [50, 100, 200]
 
-    #### Loading data
+    # Loading data
     X_train, y_train, X_test, y_test = load_data(data_dir_path)
     class_names = np.unique(y_test)
-
 
     for n_comps in PCA_comps:
         # Creating our pipeline
@@ -122,41 +112,59 @@ def main():
             SVC(random_state=random_state),
             verbose=False
         )
-        print(f'Finding best hyperparameters for MNIST, n_comps = {n_comps}')
+        print(f'Finding best hyperparameters for {dataset_name} with n_comps = {n_comps}')
 
         # 1 row, 3 columns of subplots
-        fig, axes = plt.subplots(1, 3, figsize=(18, 12))
+        fig, axes = plt.subplots(1, 3, figsize=(16, 4))
 
         for i, param_list in enumerate(params):
-            #### Get the best model with parameters in param_list
-            model = grid_search(
-                pipeline, param_list, X_train[:1000], y_train[:1000])
+            # Get the best model with parameters in param_list
+            model, time_cost = grid_search(
+                pipeline, param_list, X_train[:subset], y_train[:subset])
+
             accuracy = model.score(X_test, y_test)
             cm = confusion_matrix(y_test, model.predict(X_test))
 
-            #### Adding values to the table
+            # Adding values to the table
             table['PCA Comps'].append(n_comps)
             table['Kernel'].append(param_list['svc__kernel'][0])
             table['Accuracy'].append(accuracy)
-            
-            
-            #### creating an image of a confusion matrix
+            table['Time Cost'].append(time_cost)
+
+            # creating an image of a confusion matrix
             sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
                         xticklabels=class_names,
                         yticklabels=class_names,
                         ax=axes[i])
-            
+
             axes[i].set_title(f'{param_list["svc__kernel"][0]} Kernel')
             axes[i].set_ylabel('True label')
             axes[i].set_xlabel('Predicted label')
-            
-        fig.suptitle(f'MNIST, PCA={n_comps}')
-        # plt.show()
-        plt.savefig(fname=f'./images/cm_pca_{n_comps}_{param_list['svc__kernel'][0]}_kernel')
 
-    #### Plotting the table
-    for n_comps, kernel, acc in zip(table['PCA Comps'], table['Kernel'], table['Accuracy']):
-        print(f"{n_comps} & {kernel} & {acc:.2f}  \\\\")
+        fig.suptitle(f'Confusion Matrices {dataset_name} PCA={n_comps}')
+        plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+        # plt.show()
+        plt.savefig(
+            fname=f'./images/cm_{dataset_name}_pca_{n_comps}_{param_list['svc__kernel'][0]}_kernel')
+
+    # printing the table
+    for n_comps, kernel, acc, time_cost in zip(table['PCA Comps'], table['Kernel'], table['Accuracy'], table['Time Cost']):
+        print(f"{n_comps} & {kernel} & {acc:.3f} & {time_cost:.3f} \\\\")
+
+
+
+def main():
+    mnist_data_path = './mnist-data/'
+    fashion_data_path = './fashion-data/'
+    
+    if len(sys.argv) < 2:
+        #### Run task for both datasets
+        run_task(mnist_data_path)
+        run_task(fashion_data_path)
+    elif sys.argv[1] == 'mnist':
+        run_task(mnist_data_path)
+    elif sys.argv[1] == 'fashion':
+        run_task(fashion_data_path)
 
 
 if __name__ == '__main__':

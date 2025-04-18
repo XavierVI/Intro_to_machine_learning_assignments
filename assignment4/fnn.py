5b06a94e961ce979480bbd8b2e30a07d7ff444ac
xavier <vigenesisvi@gmail.com>
2025-04-18 12:53:24 -0600

    updated code to load movie dataset and make a prediction

--- assignment4/fnn.py
+++ assignment4/fnn.py
@@ -1,21 +1,72 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader, Dataset
+
+import pandas as pd
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+
+class MovieReviewsDataset(Dataset):
+
+    def __init__(self, csv_file_path, dataset_size, transform=None):
+        # read the csv file path
+        df = pd.read_csv(csv_file_path)
+        # shrink the dataset
+        df = df[:dataset_size]
+        self.labels = torch.tensor(df['sentiment'].values)
+
+        # creating the feature vector
+        tfidf_vectorizer = TfidfVectorizer(
+            tokenizer=self.tokenizer,
+            # max_features=10_000
+            stop_words='english'
+        )
+        features = tfidf_vectorizer.fit_transform(df['review'])
+        self.features = torch.tensor(features.toarray(), dtype=torch.float32)
+        self.transform = transform
+
+    def num_of_features(self):
+        return self.features.shape[1]
+
+    def tokenizer(self, text):
+        return text.split()
+
+    def __len__(self):
+        return self.features.shape[0]
+
+    def __getitem__(self, idx):
+        X = self.features[idx]
+        y = self.labels[idx]
+
+        if self.transform:
+            X = self.transform(X)
+
+        return X, y
+
 
 
 class FNN(nn.Module):
-    def __init__(self):
+    def __init__(self, input_size):
         super().__init__()
         
         self.layers = nn.Sequential(
-            nn.Linear(10, 10),
-            nn.ReLU()
+            nn.Linear(input_size, 100),
+            nn.ReLU(),
+            nn.Linear(100, 50),
+            nn.ReLU(),
+            nn.Linear(50, 2),
+            nn.ReLU(),
+            nn.Softmax(dim=1)
         )
         
 
     def forward(self, x):
-        return self.layers(x)
+        return torch.argmax(self.layers(x))
+
 
 
 def train_model(model: nn.Module, dataloader, device):
@@ -56,9 +107,16 @@ def main():
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     print(device)
     
+
+    dataset = MovieReviewsDataset('./movie_data.csv', 10)
+
     #### instantiate the model and load it on the device
-    model = FNN()
+    model = FNN(dataset.num_of_features())
     model.to(device)
+
+    X, y = dataset[:1]
+    X = X.to(device)
+    print(model(X))
     
 
 
--- assignment4/move_reviews.py
+++ assignment4/move_reviews.py
@@ -3,33 +3,50 @@ import os
 import sys
 import pyprind
 
-import numpy as np
+import re
 
 
-basepath = 'aclImdb'
 
-labels = {'pos': 1, 'neg': 0}
+def preprocessor(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
+    text = (re.sub('[\W]+', ' ', text.lower()) +
+            ' '.join(emoticons).replace('-', ''))
+    return text
 
-pbar = pyprind.ProgBar(50000, stream=sys.stdout)
 
-df = pd.DataFrame()
+def build_csv():
+    basepath = 'aclImdb'
+    labels = {'pos': 1, 'neg': 0}
+    pbar = pyprind.ProgBar(50_000, stream=sys.stdout)
+    data = []
 
-for s in ('test', 'train'):
-    for l in ('pos', 'neg'):
-        path = os.path.join(basepath, s, l)
-        
-        for file in sorted(os.listdir(path)):
-            with open(os.path.join(path, file), 'r', encoding='utf-8') as infile:
-                txt = infile.read()
-            df = df.append([[txt, labels[l]]], ignore_index=True)
-            pbar.update()
+    for dataset in ('test', 'train'):
+        for sentiment in ('pos', 'neg'):
+            path = os.path.join(basepath, dataset, sentiment)
+            
+            for file in sorted(os.listdir(path)):
+                with open(os.path.join(path, file), 'r', encoding='utf-8') as infile:
+                    txt = infile.read()
+                # The append method for dataframe is deprecated and is not used
+                # anymore
+                data.append([txt, labels[sentiment]])
+                pbar.update()
 
-df.columns = ['review', 'sentiment']
+    df = pd.DataFrame(data, columns=['review', 'sentiment'])
 
-df = df.reindex(np.random.permutation(df.index))
-df.to_csv('movie_data.csv', index=False, encoding='utf-8')
+    # shuffle the dataset using the sample method
+    df = df.sample(frac=1, random_state=1).reset_index(drop=True)
+    
+    #### remove HTML tags and emoticons
+    df['review'] = df['review'].apply(preprocessor)
 
-#### Testing the dataset was saved properly
-df = pd.read_csv('movie_data.csv', encoding='utf-8')
-df = df.rename(columns={"0": "review", "1": "sentiment"})
-print(df.head(3))
+    #### save data to a csv
+    df.to_csv('movie_data.csv', index=False, encoding='utf-8')
+
+
+
+build_csv()
+
+
+

126cf087c5c6e2b30bc17410398ea5841da47fbc
amlc1 <amlc1611@gmail.com>
2025-04-20 18:58:28 -0600

    perform_kfold function

--- assignment4/kfold.py
+++ assignment4/kfold.py
@@ -1,13 +1,9 @@
 import os
 import torch
 from torch import nn
-from torchvision.datasets import MNIST
-from torch.utils.data import DataLoader, ConcatDataset
-from torchvision import transforms
+from torch.utils.data import DataLoader, TensorDataset, Subset
 from sklearn.model_selection import KFold
 from fnn import FNN, train_model, load_movie_reviews
-from torch.utils.data import DataLoader, TensorDataset, Subset
-
 
 def reset_weights(m):
   '''
@@ -18,31 +14,28 @@ def reset_weights(m):
    if hasattr(layer, 'reset_parameters'):
     print(f'Reset trainable parameters of layer = {layer}')
     layer.reset_parameters()
-  
-  
-if __name__ == '__main__':
-  
-  # Configuration options
-  k_folds = 5
-  num_epochs = 6
-  loss_function = nn.CrossEntropyLoss()
-  
+
+def perform_kfold(
+        dataset,
+        model_class,
+        input_size,
+        k_folds=5,
+        num_epochs=50,
+        batch_size=64,
+        learning_rate=1e-5,
+        l2_reg=0.0001):
+
   # For fold results
   results = {}
   
   # Set fixed random number seed
   torch.manual_seed(42)
   
-  
   # Define the K-fold Cross Validator
   kfold = KFold(n_splits=k_folds, shuffle=True)
 
-  batch_size = 64
-  dataset_size = 50_000
-  max_features =  20_000
-
-  X, y = load_movie_reviews('./movie_data.csv', dataset_size, max_features)
-  dataset = TensorDataset(X, y)
+  # X, y = load_movie_reviews('./movie_data.csv', dataset_size, max_features)
+  # dataset = TensorDataset(X, y)
     
   # Start print
   print('--------------------------------')
@@ -60,6 +53,7 @@ if __name__ == '__main__':
 
     train_dataset = Subset(dataset, train_ids)
     test_dataset = Subset(dataset, test_ids)
+
     train_loader = DataLoader(
         train_dataset,
         batch_size=batch_size,
@@ -72,61 +66,30 @@ if __name__ == '__main__':
     )
 
     #### instantiate the model and load it on the device
-    model = FNN(X.size(1))
+    # model = FNN(X.size(1))
+    # model.to(device)
+    # model.apply(reset_weights)
+    model = model_class(input_size)
     model.to(device)
     model.apply(reset_weights)
-    
-    # Initialize optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-    
-    # Run the training loop for defined number of epochs
-    for epoch in range(0, num_epochs):
 
-      # Print epoch
-      print(f'Starting epoch {epoch+1}')
+    train_model(
+      model=model,
+      train_loader=train_loader,
+      test_loader=test_loader,
+      device=device,
+      num_epochs=num_epochs,
+      learning_rate=learning_rate,
+      l2_reg=l2_reg
+    )
 
-      # Set current loss value
-      current_loss = 0.0
-
-      # Iterate over the DataLoader for training data
-      for i, data in enumerate(train_loader, 0):
-        
-        # Get inputs
-        inputs, targets = data
-        
-        # Zero the gradients
-        optimizer.zero_grad()
-        
-        # Perform forward pass
-        outputs = model(inputs)
-        
-        # Compute loss
-        loss = loss_function(outputs, targets)
-        
-        # Perform backward pass
-        loss.backward()
-        
-        # Perform optimization
-        optimizer.step()
-        
-        # Print statistics
-        current_loss += loss.item()
-        if i % 500 == 499:
-            print('Loss after mini-batch %5d: %.3f' %
-                  (i + 1, current_loss / 500))
-            current_loss = 0.0
-            
     # Process is complete.
     print('Training process has finished. Saving trained model.')
 
     # Print about testing
     print('Starting testing')
-    
-    # Saving the model
-    save_path = f'./model-fold-{fold}.pth'
-    torch.save(model.state_dict(), save_path)
 
-    # Evaluationfor this fold
+    # Evaluation for this fold
     correct, total = 0, 0
     with torch.no_grad():
 
@@ -135,6 +98,7 @@ if __name__ == '__main__':
 
         # Get inputs
         inputs, targets = data
+        inputs, targets = inputs.to(device), targets.to(device)
 
         # Generate outputs
         outputs = model(inputs)
@@ -145,15 +109,40 @@ if __name__ == '__main__':
         correct += (predicted == targets).sum().item()
 
       # Print accuracy
-      print('Accuracy for fold %d: %d %%' % (fold, 100.0 * correct / total))
+      accuracy = 100.0 * correct / total
+      print('Accuracy for fold %d: %d %%' % (fold, accuracy))
       print('--------------------------------')
-      results[fold] = 100.0 * (correct / total)
+      results[fold] = accuracy
     
   # Print fold results
+  total_accuracy = 0.0
+  for value in results.values():
+    total_accuracy += value
+  avg_accuracy = total_accuracy / len(results)
+
   print(f'K-FOLD CROSS VALIDATION RESULTS FOR {k_folds} FOLDS')
   print('--------------------------------')
-  sum = 0.0
   for key, value in results.items():
     print(f'Fold {key}: {value} %')
-    sum += value
-  print(f'Average: {sum/len(results.items())} %')
+  print(f'Average: {avg_accuracy} %')
+
+  return results, avg_accuracy
+
+
+if __name__ == '__main__':
+  # Load your data
+  dataset_size = 50_000
+  max_features = 20_000
+  X, y = load_movie_reviews('./movie_data.csv', dataset_size, max_features)
+  dataset = TensorDataset(X, y)
+
+  # Call the k-fold cross validation function
+  results, avg_accuracy = perform_kfold(
+    dataset=dataset,
+    model_class=FNN,
+    input_size=X.size(1),
+    k_folds=5,
+    num_epochs=50,
+    batch_size=64
+  )
+

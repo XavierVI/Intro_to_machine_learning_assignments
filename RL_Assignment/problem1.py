d6131f6dce9fb611ede872f64e9b543edfa71a10
xavier <vigenesisvi@gmail.com>
2025-04-30 12:45:14 -0600

    added Q learning algorithm for problem 2

--- RL_Assignment/problem1.py
+++ RL_Assignment/problem1.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.integrate import odeint
+import pyprind
+
 """
 
 """
@@ -10,7 +12,7 @@ class Agent:
     This class defines the agent for problem 1.
     """
 
-    def __init__(self):
+    def __init__(self, state_space_size=21):
         self.car = CarModel()
         self.Q = np.zeros((21, 21, 13))
         self.alpha = 0.1
@@ -20,8 +22,8 @@ class Agent:
         self.epsilon_min = 0.01
 
         # state space and action space
-        self.X = np.linspace(-5, 5, 21)
-        self.V = np.linspace(-5, 5, 21)
+        self.X = np.linspace(-5, 5, state_space_size)
+        self.V = np.linspace(-5, 5, state_space_size)
         self.U = np.array([-5, -1, -0.1, -0.01, -0.001, -0.0001, 0,
                            0.0001, 0.001, 0.01, 0.1, 1, 5])
 
@@ -37,6 +39,7 @@ class Agent:
         """
         Discretizes the state s = (x, v) to the nearest values in X and V.
         """
+        # clip values to keep them within -5 and 5
         x = s[0]
         v = s[1]
         x_discrete = self.X[np.argmin(np.abs(self.X - x))]
@@ -59,6 +62,33 @@ class Agent:
         best_action = self.U[best_action_idx]
 
         return best_action
+
+    def update_Q(self, s, u, r, next_s):
+        """
+        This function updates the Q table using the update method
+        from the slides.
+
+        s: the state when the action was made
+        u: the action taken
+        r: the reward received
+        next_s: the state after the action was taken
+        """
+        x_idx = self.s_to_index[s[0]]
+        v_idx = self.s_to_index[s[1]]
+        u_idx = self.u_to_index[u]
+        next_x_idx = self.s_to_index[next_s[0]]
+        next_v_idx = self.s_to_index[next_s[1]]
+
+        # get the maximum Q value for the next state
+        max_next_s = np.max(
+            self.Q[next_x_idx, next_v_idx, :]
+        )
+        
+        # update the Q table
+        self.Q[x_idx, v_idx, u_idx] += \
+            self.alpha * (r + self.gamma * max_next_s - self.Q[x_idx, v_idx, u_idx])
+
+        pass
 
 
 class CarModel:
@@ -177,10 +207,54 @@ class CarModel:
 
         return trajectory
             
-agent = Agent()
-s0 = (1, -1)
 
-env = CarModel()
-T = 10
-trajectory = env.generate_trajectory(s0, T, agent)
-print(trajectory)
+def Q_learning(agent: Agent, env: CarModel, num_episodes=50, time_steps=10):
+    """
+    This function is an implementation of the Q-learning algorithm.
+
+    num_episodes: the number of episodes to run
+    
+    """
+    history = []
+    pbar = pyprind.ProgBar(num_episodes, title="Training", width=40)
+
+    for episode in range(num_episodes):
+        # randomly select the initial state from X and V
+        random_idx1 = np.random.default_rng(1).choice(agent.X.size)
+        random_idx2 = np.random.default_rng(1).choice(agent.V.size)
+        s_i = (agent.X[random_idx1], agent.V[random_idx2])
+
+        final_reward = 0
+
+        for t in range(time_steps):
+            u = agent.get_best_action(s_i)
+            next_s = agent.discretize_state(env.get_next_state(s_i, u))
+            reward = env.get_reward(s_i, u)
+
+            print(f"Episode {episode}, Time step {t}: s = {s_i}, u = {u}, r = {reward}")
+            agent.update_Q(s_i, u, reward, next_s)
+            
+            s_i = next_s
+            final_reward += reward
+
+            if s_i[0] == 0 and s_i[1] == 0:
+                # if the car is at the goal, break
+                break
+
+            elif abs(s_i[0]) >= 5 or abs(s_i[1]) >= 5:
+                # if the car is out of bounds, break
+                break
+        
+        history.append(final_reward)
+        pbar.update(1)
+
+    pbar.stop()
+
+    return history
+
+        
+
+agent = Agent(state_space_size=21)
+environment = CarModel()
+history = Q_learning(agent, environment, num_episodes=50, time_steps=10)
+print(history)

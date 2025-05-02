8e1ebffbab975650666a5535e25a0b4a35155d25
amlc1 <amlc1611@gmail.com>
2025-05-02 05:16:41 -0600

    Class for the agent

---
+++ Final_Problem2/task2.3.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+class Agent:
+    def __init__(self, environment, alpha=0.1, gamma=0.9, epsilon=0.1):
+        self.environment = environment
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+
+        self.q_table = np.zeros((environment.height, environment.width, len(environment.actions)))
+
+        def choose_action(self, state):
+            """
+            Choose an action using epsilon - greedy policy.
+            """
+            row, col = state
+
+            if np.random.uniform() < self.epsilon:
+                return np.random.randint(0, len(self.env.actions))
+
+            return np.argmax(self.q_table[row, col])
+
+        def update_q_table(self, state, action, reward, next_state):
+            """
+            Update Q-table using the Q-learning update rule.
+            """
+            row, col = state
+            next_row, next_col = next_state
+
+            td_target = reward + self.gamma * np.max(self.q_table[next_row, next_col, :])
+
+            td_error = td_target - self.q_table[row, col, action]
+
+            # Update Q-value
+            self.q_table[row, col, action] += self.alpha * td_error
+
+
+
+
+
+
+
+

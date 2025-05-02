7ae7c1f7310b9ec8b5fd851c7f79bf1c9939a716
paz103808 <paz103808@unm.edu>
2025-05-02 16:42:48 -0600

    updated envoirnment

--- Final_Problem2/task2.2.py
+++ Final_Problem2/task2.2.py
@@ -35,5 +35,30 @@ class Envoirnment:
         y = self.state
         delta_x = x + dx
         delta_y = y + dy
+        # Check bounds and for wall
+        if self.is_valid_move(delta_x, delta_y):
+            self.state = (delta_x, delta_y)
 
-        return self.state
+        reward = self.get_reward(self.state)
+        done = self.state == self.goal
+
+        return self.state, reward, done
+    
+    def is_valid_move(self, x, y):
+        in_bounds = 0 <= x < self.grid.shape[0] and 0 <= y < self.grid.shape[1]
+        return in_bounds and self.grid[x, y] == 1  # 1 means free
+    
+
+map_1 = Image.open("/Users/alanpaz/Documents/CS429/CS429_Assignment1-1/Final_Problem2/maps/map1_compressed(48,48).bmp") # 532 x 528
+map_2 = Image.open("/Users/alanpaz/Documents/CS429/CS429_Assignment1-1/Final_Problem2/maps/map2_compressed(48,48).bmp") # 532 x 528
+bw_img1 = map_1.convert('1')  # Convert to 1-bit black & white
+bw_img2 = map_2.convert('1')  # Convert to 1-bit black & white
+
+grid1 = np.array(bw_img1)
+grid2 = np.array(bw_img2)
+
+start = (0, 0)  # Pick a valid start coordinate
+goal = (40, 47)  # Pick a valid goal coordinate
+
+env1 = Envoirnment(grid=grid1, start=start, goal=goal)
+env2 = Envoirnment(grid=grid2, start=start, goal=goal)
